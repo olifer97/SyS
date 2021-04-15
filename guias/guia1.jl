38c2360504a904d6cc8819745715e324b60db686
Oli <olifer97@gmail.com>
2021-04-14 23:52:02 -0300

    hasta el ejercicio 6 inclusive pero con algunos problemillas a revisar en el medio.

--- guias/guia1.jl
+++ guias/guia1.jl
@@ -4,6 +4,15 @@
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
 # ╔═╡ 161ba131-c7c4-41f4-bdc8-25e9346f0f4f
 using SyS
 
@@ -65,7 +74,141 @@ sum(n -> exp(im* n * π/2), -2:7)
 sum(n -> (0.5^n) * exp(im* n * π/2), 0:9)
 
 # ╔═╡ ae7086d7-65fb-4f1a-bb2a-967b119fe966
+# d
+sum(n -> cos(n * π/2), 0:9)
 
+# ╔═╡ 8dbfcccc-9094-4247-b765-6b0627d014ee
+md"""
+## Ejercicio 3
+"""
+
+# ╔═╡ 45e5c686-d3c8-4c4f-aab2-66a43d2c0563
+md"""
+a3 = $(@bind a3 Slider(-0.5:0.5:2; show_value=true, default=2))
+"""
+
+# ╔═╡ 835cb21d-fd3c-4892-ba0a-5500f424a79e
+y = n -> sum(N -> a3^N, 0:n) * u(n)
+
+# ╔═╡ f71f6ff0-0911-4593-a5f9-6528fecf7733
+# a
+y(3)
+
+# ╔═╡ e9fc1dce-4353-499d-b67f-ea1b5e732afe
+# b
+ y(2000)
+
+# ╔═╡ 483435d8-59d4-4c28-b4db-5da1ed3cfbe9
+# c (TODO: Arreglar bug con Inf)
+y(Inf)
+
+# ╔═╡ 4700a652-7ab9-41e5-b96a-008319550361
+# d -> Mover slider
+
+# ╔═╡ 2af44b15-8cdd-468a-80f8-9d06118947ac
+# e -> Mover slider (TODO: Arreglar bug con Inf)
+
+# ╔═╡ 080cac50-0604-48c7-a6b2-779fa9ca7eb4
+md"""
+## Ejercicio 4
+"""
+
+# ╔═╡ 49f3371a-cfe7-49d2-b15e-4bd41a99bcef
+md"""
+A = $(@bind A Slider(0:10; show_value=true, default=1))
+
+N = $(@bind N Slider(1:100; show_value=true, default=2))
+"""
+
+# ╔═╡ cc94a15b-b0d8-475c-ad61-503f89ee133e
+x4 = n -> A*(u(n)*u(N - n)) #MAAAAAL
+
+# ╔═╡ 464b36f6-5ca2-44b5-b27d-e84ea48a3d44
+function pulso(n) #MAAAAL
+	k = (N * round(n/N))+1
+	println(k)
+	n%N == 0 && return A
+	return -A
+end
+
+# ╔═╡ 580f2224-c54b-445c-bb38-422fe045b8f5
+pulso(0)
+
+# ╔═╡ 33f47f8f-4722-45b2-9b27-c3ff9ef4975e
+stem(-2*N:2*N, pulso.(-2*N:2*N))
+
+# ╔═╡ 9845d730-1422-48b3-ac29-c461cecfcd3e
+md"""
+## Ejercicio 5
+"""
+
+# ╔═╡ 3a7a6e2e-f555-4464-b074-d6cf0b2d9e8b
+function xn(n)
+	n < 10 && return 1
+	n >= 10 && n < 20 && return 2-(n/10)
+	return 0
+end
+
+# ╔═╡ fa4dd6fa-0f3f-48c4-b31c-16bb82b1354f
+stem(0:25, xn.(0:25))
+
+# ╔═╡ fea913be-9771-4d70-a7bf-a678bc18824e
+# a
+let
+	a = n -> xn(3 * n / 2 + 1)
+	stem(0:25, a.(0:25))
+end
+
+# ╔═╡ 9da46f08-9d01-4730-9710-b0fb83ae8bee
+# b
+let
+	b = n -> xn((-2*n)-1) 
+	stem(0:25, b.(0:25))
+end
+
+# ╔═╡ ecebe642-b296-416f-a598-edbcf1951e3c
+# c
+let
+	c = n -> xn(n*0.5 - 0.5)
+	stem(0:25, c.(0:25))
+end
+
+# ╔═╡ 60b7346c-a0ab-4c95-9180-62f1d15dfb5e
+md"""
+## Ejercicio 6
+"""
+
+# ╔═╡ c27c07ce-fb7a-46f7-9a89-698025d68ffa
+function xn8(n)
+	(n >= 4 || n <= -3) && return 0
+	n >= -2 && n <= 0 && return n + 3
+	n >=1 && n <=2 && return 2
+	return -n + 4
+end
+
+# ╔═╡ 96d90520-3145-4c27-a821-3fe9fd9a3e59
+stem(-8:8, xn8.(-8:8))
+
+# ╔═╡ b4c91ffd-a1dc-42b6-bbfb-e3026ff48846
+# a
+let
+	a = n -> xn8(n) * u(2-n)
+	stem(-8:8, a.(-8:8))
+end
+
+# ╔═╡ 7ea80b1e-cecd-49e8-9e6c-a56903595d46
+# b
+let
+	b = n -> xn8(n-1)*δ(n-3) 
+	stem(-8:8, b.(-8:8))
+end
+
+# ╔═╡ 049b6a08-a092-4d9d-ae9b-d222f83930d4
+# c
+let
+	c = n -> 0.5*xn8(n) + (0.5 * (-1)^n * xn8(n))
+	stem(-8:8, c.(-8:8))
+end
 
 # ╔═╡ Cell order:
 # ╠═161ba131-c7c4-41f4-bdc8-25e9346f0f4f
@@ -86,3 +229,29 @@ sum(n -> (0.5^n) * exp(im* n * π/2), 0:9)
 # ╠═3d60652c-bd45-4b4d-aa5b-72c9f9670a60
 # ╠═35959a94-b547-4b9b-8731-1af44aaeb2c0
 # ╠═ae7086d7-65fb-4f1a-bb2a-967b119fe966
+# ╟─8dbfcccc-9094-4247-b765-6b0627d014ee
+# ╠═45e5c686-d3c8-4c4f-aab2-66a43d2c0563
+# ╠═835cb21d-fd3c-4892-ba0a-5500f424a79e
+# ╠═f71f6ff0-0911-4593-a5f9-6528fecf7733
+# ╠═e9fc1dce-4353-499d-b67f-ea1b5e732afe
+# ╠═483435d8-59d4-4c28-b4db-5da1ed3cfbe9
+# ╠═4700a652-7ab9-41e5-b96a-008319550361
+# ╠═2af44b15-8cdd-468a-80f8-9d06118947ac
+# ╠═080cac50-0604-48c7-a6b2-779fa9ca7eb4
+# ╠═49f3371a-cfe7-49d2-b15e-4bd41a99bcef
+# ╠═cc94a15b-b0d8-475c-ad61-503f89ee133e
+# ╠═464b36f6-5ca2-44b5-b27d-e84ea48a3d44
+# ╠═580f2224-c54b-445c-bb38-422fe045b8f5
+# ╠═33f47f8f-4722-45b2-9b27-c3ff9ef4975e
+# ╠═9845d730-1422-48b3-ac29-c461cecfcd3e
+# ╠═3a7a6e2e-f555-4464-b074-d6cf0b2d9e8b
+# ╠═fa4dd6fa-0f3f-48c4-b31c-16bb82b1354f
+# ╠═fea913be-9771-4d70-a7bf-a678bc18824e
+# ╠═9da46f08-9d01-4730-9710-b0fb83ae8bee
+# ╠═ecebe642-b296-416f-a598-edbcf1951e3c
+# ╠═60b7346c-a0ab-4c95-9180-62f1d15dfb5e
+# ╠═c27c07ce-fb7a-46f7-9a89-698025d68ffa
+# ╠═96d90520-3145-4c27-a821-3fe9fd9a3e59
+# ╠═b4c91ffd-a1dc-42b6-bbfb-e3026ff48846
+# ╠═7ea80b1e-cecd-49e8-9e6c-a56903595d46
+# ╠═049b6a08-a092-4d9d-ae9b-d222f83930d4
